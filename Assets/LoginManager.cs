5abe56ea74dd039355873abf2d6c3bebb73e7872
MattCress99 <1834243@students.wits.ac.za>
2024-04-04 21:47:00 +0200

    updated network functionality to allow for spawn into base lobby and main lobby

--- Assets/LoginManager.cs
+++ Assets/LoginManager.cs
@@ -2,20 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
-
+using TMPro;
 public class LoginManager : MonoBehaviourPunCallbacks
 {
+    public TMP_InputField PlayerName_InputName;
+
     #region Unity Methods
     // Start is called before the first frame update
     void Start()
-    {
-        PhotonNetwork.ConnectUsingSettings();
+    {   
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    #endregion
+
+    #region UI Callback Methods
+    public void ConnectAnonymously()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public void ConnectToPhotonServer()
+    {
+        if(PlayerName_InputName != null)
+        {
+            PhotonNetwork.NickName = PlayerName_InputName.text;
+            PhotonNetwork.ConnectUsingSettings();
+        }
     }
     #endregion
 
@@ -27,7 +44,8 @@ public class LoginManager : MonoBehaviourPunCallbacks
 
     public override void OnConnectedToMaster()
     {
-        Debug.Log("Connected to Master Server!");
+        Debug.Log("Connected to Master Server with player name: " + PhotonNetwork.NickName);
+        PhotonNetwork.LoadLevel("HomeScene");
     }
     
     #endregion
